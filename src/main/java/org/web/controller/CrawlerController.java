6b8b6927469e0a0aa145e1166f0e847e49c4de23
Apurva Bhoite <bhoiteapurva7@gmail.com>
2020-11-02 13:34:00 +0530

    Updated CrawlerController java file

--- src/main/java/org/web/controller/CrawlerController.java
+++ src/main/java/org/web/controller/CrawlerController.java
@@ -1,14 +1,24 @@
 package org.web.controller;
 
+import java.net.MalformedURLException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.web.crawler.implement.Crawler;
+import org.web.crawler.implement.SimpleCrawler;
 
 @RestController
 public class CrawlerController {
 
-	@RequestMapping("/crawl") 
-	public String crawl()
+	@RequestMapping(value = "/crawl", method = RequestMethod.GET, produces="text/plain")
+	public String crawl() throws MalformedURLException
 	{
-		return "Hello crawler";
+	
+		Crawler crawler = new SimpleCrawler("https://redhat.com", 2);
+		crawler.crawl("https://redhat.com");
+		
+		//return crawler.getTree().toOutput(0);
+		return crawler.getTree().toOutput(0);
 	}
 }
