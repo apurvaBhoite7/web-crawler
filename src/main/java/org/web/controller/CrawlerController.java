c957ee61702d20d762be1ee3b3b5481c64c56eea
Apurva Bhoite <bhoiteapurva7@gmail.com>
2020-11-02 22:03:14 +0530

    fixed NPE and added url as a query string

--- src/main/java/org/web/controller/CrawlerController.java
+++ src/main/java/org/web/controller/CrawlerController.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.web.crawler.implement.Crawler;
 import org.web.crawler.implement.SimpleCrawler;
@@ -11,14 +12,12 @@ import org.web.crawler.implement.SimpleCrawler;
 @RestController
 public class CrawlerController {
 
-	@RequestMapping(value = "/crawl", method = RequestMethod.GET, produces="text/plain")
-	public String crawl() throws MalformedURLException
-	{
-	
-		Crawler crawler = new SimpleCrawler("https://redhat.com", 2);
-		crawler.crawl("https://redhat.com");
-		
-		//return crawler.getTree().toOutput(0);
+	@RequestMapping(value = "/crawl", method = RequestMethod.GET, produces = "text/plain")
+	public String crawl(@RequestParam(name = "url") String url) throws MalformedURLException {
+
+		Crawler crawler = new SimpleCrawler(url, 2);
+		crawler.crawl(url);
+
 		return crawler.getTree().toOutput(0);
 	}
 }
