be1c21b9ed68856003301026a723f125b5cec240
Apurva Bhoite <bhoiteapurva7@gmail.com>
2020-11-13 08:50:51 +0530

    Updated client HttpRequest by adding depth to uri

--- src/main/java/org/web/Client.java
+++ src/main/java/org/web/Client.java
@@ -2,6 +2,7 @@ package org.web;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -11,13 +12,16 @@ public class Client {
 
 	private static final String REQUEST_URL = "http://localhost:8080/crawl";
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
 
 		Scanner scannerObj = new Scanner(System.in);
+
 		String userUrl = scannerObj.nextLine();
+		int userdepth = scannerObj.nextInt();
 
 		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(REQUEST_URL + "?url=" + userUrl)).build();
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create(REQUEST_URL + "?url=" + userUrl + "&depth=" + userdepth)).build();
 
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
