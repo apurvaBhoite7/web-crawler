b6fedb678c84847b11326aa09cd25aecd58cdb82
Apurva Bhoite <bhoiteapurva7@gmail.com>
2020-11-13 08:54:43 +0530

    Updated CrawlerController by adding depth conditions

--- src/main/java/org/web/controller/CrawlerController.java
+++ src/main/java/org/web/controller/CrawlerController.java
@@ -2,9 +2,11 @@ package org.web.controller;
 
 import java.net.MalformedURLException;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.web.crawler.implement.Crawler;
 import org.web.crawler.implement.SimpleCrawler;
@@ -13,11 +15,20 @@ import org.web.crawler.implement.SimpleCrawler;
 public class CrawlerController {
 
 	@RequestMapping(value = "/crawl", method = RequestMethod.GET, produces = "text/plain")
-	public String crawl(@RequestParam(name = "url") String url) throws MalformedURLException {
+	@ResponseBody
+	public String crawl(@RequestParam(name = "url") String url, @RequestParam(name = "depth") int depth)
+			throws MalformedURLException {
 
-		Crawler crawler = new SimpleCrawler(url, 2);
-		crawler.crawl(url);
+		if (depth >= 0 && depth <= 10) {
 
-		return crawler.getTree().toOutput(0);
+			Crawler crawler = new SimpleCrawler(url, depth);
+			crawler.crawl(url);
+
+			return crawler.getTree().toOutput(0);
+
+		} else {
+			return "Enter the depth in range of 0 to 10";
+		}
 	}
+
 }
