18ddf244fced0727e4be12fe8fda9aeb23e76954
Apurva Bhoite <bhoiteapurva7@gmail.com>
2020-11-23 06:23:10 +0530

    Updated MultithreadedCrawler

--- src/main/java/org/web/multithreaded/crawler/MultiThreadedCrawler.java
+++ src/main/java/org/web/multithreaded/crawler/MultiThreadedCrawler.java
@@ -3,26 +3,27 @@ package org.web.multithreaded.crawler;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MultiThreadedCrawler{
+public class MultiThreadedCrawler {
 
 	private TreeNode tree = new TreeNode(null);
 	TreeNode links = new TreeNode(null);
 
-	public TreeNode crawl(String url) {
+	public TreeNode crawl(String url) throws InterruptedException {
 		if (tree.data == null) {
 			tree.data = url;
 		}
 		CrawlerThread worker = new CrawlerThread(url, tree);
-		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
 		executorService.execute(worker);
 		executorService.shutdown();
-		return tree;		
-		
+		return worker.getResult();
+
 	}
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws InterruptedException {
 		MultiThreadedCrawler crawler = new MultiThreadedCrawler();
-		crawler.crawl("http://www.redhat.com");
-}
+		System.out.println(crawler.crawl("http://www.redhat.com"));
+
+	}
 
 }
